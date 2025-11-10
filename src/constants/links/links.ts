9de4e94dc3304ad8a96ee79cddc9e54fa89f1236
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-11-10 13:17:01 +0100

    typescript changes

---
+++ src/constants/links/links.ts
@@ -0,0 +1,5 @@
+export const authUrl = {
+    loginUrl:"/api/v1/login",
+    signupUrl:"/api/v1/register"
+}
+
---
+++ src/constants/types/authTypes.ts
@@ -0,0 +1,14 @@
+export interface LoginFormTypes {
+    email:string,
+    password:string
+}
+
+export interface RegisterFormTypes {
+    firstname:string,
+    lastname:string,
+    email:string,
+    phone:number,
+    password:string,
+    conf_password:string
+}
+
