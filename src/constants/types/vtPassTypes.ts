ee9b8f0469d7037abde186573e455a6879f34e77
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-11-16 00:15:42 +0100

    vt pass types for the products i want to use

---
+++ src/constants/types/vtPassTypes.ts
@@ -0,0 +1,78 @@
+
+export interface queryVTService {
+    request_id:string,
+}
+
+export interface Airtime {
+    request_id:string,
+    serviceID:string,
+    phone:number,
+    amount:number,
+}
+
+export interface Data {
+    request_id:string,
+    serviceID:string,
+    billersCode:string,
+    variation_code:string,
+    amount:number,
+    phone:number,
+}
+
+export interface CableTv {
+    request_id:string,
+    serviceID:string,
+    billersCode:string,
+    variation_code:string,
+    amount:number,
+    phone:number,
+    subscription_type:string,
+    quantity?:number,
+}
+
+export interface ElectricityMeterValidation {
+    billersCode:string,
+    serviceID:string,
+    type:string,
+}
+
+export interface ElectricityPrepaid {
+    request_id:string,
+    serviceID:string,
+    billersCode:string,
+    variation_code:string,
+    amount:number,
+    phone:number,
+}
+
+export interface ElectricityPostpaid {
+    request_id:string,
+    serviceID:string,
+    billersCode:string,
+    variation_code:string,
+    amount:number,
+    phone:number,
+}
+
+export interface Education_Waec_Check {
+    request_id:string,
+    serviceID:string,
+    variation_code:string,
+    quantity?:number,
+    phone:number,
+}
+
+export interface Education_Jamb_Profile_verification {
+    billersCode:string,
+    serviceID:string,
+    type:string,
+}
+
+export interface Education_Jamb_Pin_Vending {
+    request_id:string,
+    serviceID:string,
+    variation_code:string,
+    billersCode:string,
+    amount?:number,
+    phone:number,
+}
