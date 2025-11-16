1238f5dab97dd588fb2c5437c634198ebcf7fac2
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-11-16 02:46:48 +0100

    refactored personal page

--- src/pages/Personal.tsx
+++ src/pages/Personal.tsx
@@ -4,42 +4,48 @@ import cable from '../assets/feature-img/cable.png'
 import education from '../assets/feature-img/education.png'
 import internet from '../assets/feature-img/internet.png'
 import airtime from '../assets/feature-img/airtime.png'
+import man from "../assets/feature-img/man-dialing-image.png"
 const Personal = () => {
     const navigate = useNavigate()
     const featureData = [{
         imgSrc: airtime,
-        title:"Airtime"
+        title:"Airtime",
+        to:"/dashboard/airtime"
     },{
         imgSrc:internet,
-        title:"Data"
+        title:"Data",
+        to:"/dashboard/data"
     },{
         imgSrc:electricity,
-        title:"Electricity"
+        title:"Electricity",
+        to:"/dashboard/electricity"
     },{
         imgSrc:education,
-        title:"Education"
+        title:"Waec Pin",
+        to:"/dashboard/waec"
     },{
         imgSrc:cable,
-        title:"Cable tv"
+        title:"Cable tv",
+        to:"/dashboard/cable"
     }]
     return ( 
         <div>
             {/* section 1 */}
-            <div className="bg-white text-gray-700 text-sm px-6 w-full h-fit grid grid-cols-3  mb-20">
+            <div className="bg-white text-gray-700 text-sm px-2 md:px-6 w-full h-fit grid grid-cols-1 md:grid-cols-2 gap-6">
 
-                <div className=" flex flex-col gap-4 pt-26">
-                    <h1 className="text-gray-700 text-[1.2rem] font-semibold my-2">
-                    Welcome {``}, What are you <b className="primary font-semibold ">purchasing today?</b>
+                <div className="col-span-1 flex flex-col gap-4 pt-10">
+                    <h1 className="text-gray-700 text-[1.2rem] font-semibold my-2 bg-white">
+                    Welcome {`Bright`}, What are you <b className="primary font-semibold ">purchasing today?</b>
                 </h1>
                 <div className="flex justify-between" >
                     {featureData.map((item,index)=>{
                         return(
-                            <section key={index} className="flex flex-col items-center cursor-pointer hover:scale-110" onClick={()=>navigate("/airtime")}>
+                            <div key={index} className="flex flex-col items-center cursor-pointer hover:scale-110 bg-white" onClick={()=>navigate(item.to)}>
                         {/* <span className="w-5 h-5 border-2 rounded-sm px-4 py-2 m-2" > */}
                             <img src={item.imgSrc} alt={item.title} className="w-12 h-11" />
                         {/* </span> */}
                         <p className="mt-2 poppins-regular">{item.title}</p>
-                    </section>
+                    </div>
                         )
                     })}
                     
@@ -47,7 +53,7 @@ const Personal = () => {
                 </div>
                 <input type="text" className=" py-3 px-7 border border-gray-400 rounded-sm w-full " placeholder="Search for Service" />
 
-                <section className="border primary rounded-sm my-4 py-11 px-3 text-[0.8rem]">
+                <div className="border primary rounded-sm my-4 py-11 px-3 text-[0.8rem]">
                     <p className="text-gray-700 font-semibold text-[1.2rem]/8 capitalize "><b className="primary">Save N100</b> on bill payments when you transfer directly to your meter or Decoder's Bank
                         Account. Get instant value !
                     </p>
@@ -59,17 +65,21 @@ const Personal = () => {
                         <span>No App. No website</span>
                     </span>
                     <span className="mt-5 w-full flex items-center justify-center">
-                        <button className="text-sm bg-primary px-10 py-4 rounded-sm cursor-pointer hover:bg-primary transition text-white uppercase font-bold">
+                        <button className="text-sm bg-primary px-10 py-4 rounded-sm cursor-pointer hover:bg-[#60da68] transition text-black uppercase font-bold">
                                 Get your account
                                 </button>
                     </span>
                     
-                </section>
                 </div>
-                <div className="flex items-center justify-center">
-                    <img src="pic here" alt="pic here" />
                 </div>
-                <div></div>
+                <div className="hidden md:flex col-span-1 items-center justify-center">
+                    <img 
+                        src={man} 
+                        alt="pic here" 
+                        className="w-full h-full object-contain md:object-cover"
+                    />
+                </div>
+
             </div>
         </div>
      );
