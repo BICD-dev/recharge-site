9de3c2d4b6260fd7cb11c14fef0705ba0d9b2f7b
Bright Ihegawam <bright06iheagwam@gmail.com>
2025-11-16 01:23:28 +0100

    seperated the navigaation for the landin pages and the dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,116 @@
-import './App.css'
-import {Toaster} from 'sonner'
-import {Route, Routes} from 'react-router-dom'
-// import Navbar from './components/Navbar.tsx'
-import Header from './components/Landing/Layout/Header/index.tsx'
-import Home from './pages/Home.tsx'
-import Personal from './pages/Personal.tsx'
-import About from './pages/About.tsx'
-// import Footer from './components/Footer.tsx'
-import Footer from './components/Landing/Layout/Footer/index.tsx'
-import Login from './pages/auth/Login.tsx'
-import Register from './pages/auth/Register.tsx'
-import Airtime from './pages/vtu/Airtime.tsx'
-import Data from './pages/vtu/Data.tsx'
-import Electricity from './pages/vtu/Electricity.tsx'
-import Waec from './pages/vtu/Waec.tsx'
-import Utility from './pages/vtu/Utility.tsx'
-import NotFound from './components/NotFound/index.tsx'
-import ScrollToTop from './components/ScrollToTop/index.tsx'
-function App() {
+import { Toaster } from "sonner";
+import { Routes, Route } from "react-router-dom";
 
+// Landing pages
+import Header from "./components/Landing/Layout/Header";
+import Footer from "./components/Landing/Layout/Footer";
+import Home from "./pages/Home";
+import Personal from "./pages/Personal";
+import About from "./pages/About";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+// Dashboard pages
+import Layout from "./components/Dashboard/Layout/Layout";
+import Airtime from "./pages/vtu/Airtime";
+import Data from "./pages/vtu/Data";
+import Electricity from "./pages/vtu/Electricity";
+import Utility from "./pages/vtu/Cable";
+import Waec from "./pages/vtu/Waec";
+
+import NotFound from "./components/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
+
+function App() {
   return (
     <div className="App poppins-regular">
-       <Toaster position="top-right" />
-      <Header />
+      <Toaster position="top-right" />
+      <ScrollToTop />
 
-        <Routes>
+      <Routes>
+        {/* Landing pages */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/personal"
+          element={
+            <>
+              <Header />
+              <Personal />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <>
+              <Header />
+              <About />
+              <Footer />
+            </>
+          }
+        />
 
-          <Route path='/' element={<Home />} />
-          <Route path='/personal' element={<Personal />} />
-          <Route path='/about' element={<About />} />
+        {/* Auth pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-          {/* auth paths */}
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          
-          {/* core functional routes */}
-          <Route path='/airtime' element={<Airtime/>}/>
-          <Route path='/data' element={<Data/>}/>
-          <Route path='/electricity' element={<Electricity/>}/>
-          <Route path='/utility' element={<Utility/>}/>
-          <Route path='/waec' element={<Waec/>}/>
+        {/* Dashboard / VTU pages */}
+        <Route
+          path="/airtime"
+          element={
+            <Layout title="Airtime">
+              <Airtime />
+            </Layout>
+          }
+        />
+        <Route
+          path="/data"
+          element={
+            <Layout title="Data">
+              <Data />
+            </Layout>
+          }
+        />
+        <Route
+          path="/electricity"
+          element={
+            <Layout title="Electricity">
+              <Electricity />
+            </Layout>
+          }
+        />
+        <Route
+          path="/utility"
+          element={
+            <Layout title="Utility">
+              <Utility />
+            </Layout>
+          }
+        />
+        <Route
+          path="/waec"
+          element={
+            <Layout title="WAEC">
+              <Waec />
+            </Layout>
+          }
+        />
 
-          {/* 404 page */}
-          <Route path='*' element={<NotFound/>} />
-        </Routes>
-        
-      <Footer />
-          
-
+        {/* 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/vtu/Cable.tsx
@@ -0,0 +1,9 @@
+const Cable = () => {
+    return ( 
+        <div className="h-screen flex justify-ceter items-center">
+             <p>Cable</p>
+        </div>
+     );
+}
+ 
+export default Cable;
